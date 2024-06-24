28a2fe5374c633dd9c87b78c55c86e3fb08ae835
ahmedsnippet <ahmedsnippet@gmail.com>
2024-06-24 17:25:41 +0500

    Configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
-import express from 'express'
+import express from "express";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-export {app}
+// Configuration
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+export { app };
