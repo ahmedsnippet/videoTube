6c8e26502dd232ae1452c4332679dcc556cc06f3
ahmedsnippet <ahmedsnippet@gmail.com>
2024-06-24 18:41:11 +0500

    registerUser-hashPassword

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,26 @@
-import { User } from './../model/userModel.js';
+import { User } from "./../model/userModel.js";
+import bcyrpt from "bcrypt";
 
 const registerUser = async (req, res) => {
-    const {} = req.body
-    console.log(req)
-    res.json({msg : "ok"})
-}
-export { registerUser }
+  const { email, username, password } = req.body;
+  // console.log(email)
+
+  try {
+    const salt = 10;
+
+    const hashedPassword = await bcyrpt.hash(password, salt);
+
+    const user = await User.create({
+      email: email,
+      username: username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(200).send({
+      message: "User Register Succesfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export { registerUser };
