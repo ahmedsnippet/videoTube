ed4b8b3621a1aaf74309a1c411b07c3c7b994df4
ahmedsnippet <ahmedsnippet@gmail.com>
2024-06-24 17:22:59 +0500

    Express -App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import connectDB from "./database/Database.js";
-import dotenv from 'dotenv'
+import { app } from "./app.js";
+import dotenv from "dotenv";
 dotenv.config({
-    path: './.env'
-})
-
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Is Listening At PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Some Error Occur Server Listening`);
+  });
